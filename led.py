2f6638e7c6c152cf7c1c40a1c45809a729c77e39
putara <38089082+putara@users.noreply.github.com>
2019-03-21 22:36:43 +1300

    Refactor script

--- led.py
+++ led.py
@@ -6,30 +6,14 @@ import getopt
 import string
 import fontforge
 
-cell = 24
-duty = 3
-condense = 100
-offset = 1
-space = 4
-outdir = 'bin'
-ledtxt = 'led.txt'
-
 re_comment = re.compile(r'^#')
 re_char = re.compile(r'^\:([0-9A-Fa-f]{2,6}|\.[\w]+)')
 re_lcd = re.compile(r'^([\*-\.]{5})')
 
-xcell = 0
-ycell = 0
-xgap = 0
-ygap = 0
-xoffset = 0
-yoffset = 0
-xspace = 0
-yspace = 0
-ascent = 0
-descent = 0
-svgwidth  = 0
-svgheight = 0
+cfg = dict(cell = 24, duty = 3, condense = 100, offset = 1, space = 4)
+met = dict(xcell = 0, ycell = 0, xgap = 0, ygap = 0, xoffset = 0, yoffset = 0, xspace = 0, yspace = 0, ascent = 0, descent = 0, svgwidth  = 0, svgheight = 0)
+outdir = 'bin'
+ledtxt = 'led.txt'
 
 try:
     os.mkdir(outdir)
@@ -47,15 +31,15 @@ def addglyph(cp, dots):
     empty = True
     svgfile = os.path.join(outdir, '~ledtmp.svg')
     fp = open(svgfile, 'w')
-    fp.write('<svg xmlns:svg="http://www.w3.org/2000/svg" viewBox="0 0 {0} {1}">'.format(svgwidth, svgheight))
+    fp.write('<svg xmlns:svg="http://www.w3.org/2000/svg" viewBox="0 0 {0} {1}">'.format(met['svgwidth'], met['svgheight']))
     for k, v in dots.items():
         x = k % 5
         y = k / 5
         if v:
-            x1 = xoffset + (xgap + xcell) * x
-            y1 = yoffset + (ygap + ycell) * y
-            x2 = x1 + xcell
-            y2 = y1 + ycell
+            x1 = met['xoffset'] + (met['xgap'] + met['xcell']) * x
+            y1 = met['yoffset'] + (met['ygap'] + met['ycell']) * y
+            x2 = x1 + met['xcell']
+            y2 = y1 + met['ycell']
             fp.write('  <path d="M {0},{1} V {3} H {2} V {1} Z"/>'.format(x1, y1, x2, y2))
             empty = False
     fp.write('</svg>')
@@ -65,8 +49,8 @@ def addglyph(cp, dots):
     else:
         ch = ff.createChar(-1, cp)
     ch.clear()
-    ch.width = svgwidth
-    ch.vwidth = svgheight
+    ch.width = met['svgwidth']
+    ch.vwidth = met['svgheight']
     if not empty:
         ch.importOutlines(svgfile, ('removeoverlap', 'correctdir'))
     try:
@@ -79,7 +63,7 @@ def usage():
     print('ffpython led.py [-h][-s size][-d duty][-c condense][-f offset][-l space][-o outdir] [led.txt]')
 
 def init():
-    global cell, duty, condense, offset, space, outdir, xcell, ycell, xgap, ygap, xoffset, yoffset, xspace, yspace, ascent, descent, svgwidth, svgheight
+    global cfg, met, outdir, ledtxt
 
     try:
         opts, args = getopt.getopt(sys.argv[1:], 'hs:d:c:f:l:o:')
@@ -93,30 +77,30 @@ def init():
             usage()
             return ''
         if o == '-s':
-            cell = int(a)
+            cfg['cell'] = int(a)
         if o == '-d':
-            duty = float(a)
+            cfg['duty'] = float(a)
         if o == '-c':
-            condense = float(a)
+            cfg['condense'] = float(a)
         if o == '-f':
-            offset = float(a)
+            cfg['offset'] = float(a)
         if o == '-l':
-            space = float(a)
+            cfg['space'] = float(a)
         if o == '-o':
             outdir = a
 
-    xcell = cell * duty * (0.01 * condense)
-    ycell = cell * duty
-    xgap = cell * (0.01 * condense)
-    ygap = cell
-    xoffset = xgap * offset
-    yoffset = ygap * offset
-    xspace = xgap * space
-    yspace = ygap * space
-    ascent = yoffset + (ygap + ycell) * 7
-    descent = yspace
-    svgwidth  = xoffset + (xgap + xcell) * 5 + xspace
-    svgheight = ascent + descent
+    met['xcell'] = cfg['cell'] * cfg['duty'] * (0.01 * cfg['condense'])
+    met['ycell'] = cfg['cell'] * cfg['duty']
+    met['xgap'] = cfg['cell'] * (0.01 * cfg['condense'])
+    met['ygap'] = cfg['cell']
+    met['xoffset'] = met['xgap'] * cfg['offset']
+    met['yoffset'] = met['ygap'] * cfg['offset']
+    met['xspace'] = met['xgap'] * cfg['space']
+    met['yspace'] = met['ygap'] * cfg['space']
+    met['ascent'] = met['yoffset'] + (met['ygap'] + met['ycell']) * 7
+    met['descent'] = met['yspace']
+    met['svgwidth']  = met['xoffset'] + (met['xgap'] + met['xcell']) * 5 + met['xspace']
+    met['svgheight'] = met['ascent'] + met['descent']
 
     if len(args) > 0:
         ledtxt = args[0]
@@ -126,20 +110,20 @@ def main():
     if not init():
         return
 
-    ff.ascent = ascent
-    ff.descent = descent
+    ff.ascent = met['ascent']
+    ff.descent = met['descent']
     ff.weight = 'Regular'
     ff.version = '1.00'
-    ff.comment = '-s %d -d %g -c %g -f %g -l %g' % (cell, duty, condense, offset, space)
+    ff.comment = '-s %d -d %g -c %g -f %g -l %g' % (cfg['cell'], cfg['duty'], cfg['condense'], cfg['offset'], cfg['space'])
     ff.os2_winascent_add = 0
     ff.os2_windescent_add = 0
     ff.hhea_ascent_add = 0
     ff.hhea_descent_add = 0
-    ff.os2_winascent = ascent
-    ff.os2_windescent = descent
-    ff.hhea_ascent = ascent
-    ff.hhea_descent = -descent
-    ff.hhea_linegap = yspace
+    ff.os2_winascent = met['ascent']
+    ff.os2_windescent = met['descent']
+    ff.hhea_ascent = met['ascent']
+    ff.hhea_descent = -met['descent']
+    ff.hhea_linegap = met['yspace']
 
     cp = ''
     y = 0
